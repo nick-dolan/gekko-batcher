99ba7b00a81948839d7b2d9a9fb37e706b3b5e21
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2018-12-02 22:37:38 +0300

    Check if gekko is running or not
    and if it's not show neat message in console

--- bruteforce.js
+++ bruteforce.js
@@ -351,7 +351,13 @@ function runBacktest(config) {
                     });
             }
         }).catch(function (error) {
-            log(error);
+            if (error.code === 'ECONNREFUSED') {
+               log('Gekko isn\'t running probably. Go to Gekko\'s folder and type: node gekko --ui');
+
+                process.exit(0);
+            } else {
+                log(error);
+            }
         })
     })
 }
