44c29fcb03b58e362d0445394df806789927b84d
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2019-02-24 12:58:00 +0300

    Add a progress bar to imports

--- import.js
+++ import.js
@@ -8,6 +8,44 @@ const WebSocket = require('ws')
 const { Observable } = require('rxjs')
 const chalk = require('chalk')
 const configsGenerator = require(util.dirs().tools + '/configsGenerator')
+const moment = require('moment')
+const ProgressBar = require('ascii-progress')
+
+/*
+* Observable with Proxy inside for message listenning
+* */
+let proxyMessage = { message: {} }
+
+const importMessageObservable = new Observable(subscriber => {
+  proxyMessage = new Proxy(proxyMessage, {
+    set (target, propKey, value) {
+      target[propKey] = value
+
+      subscriber.next(value)
+
+      return true
+    }
+  })
+})
+
+/*
+* Init client for WebSocket
+* */
+let api = util.removeHttp(util.config.apiUrl)
+
+const ws = new WebSocket(`ws://${api}/gekko_event`, '', {})
+
+ws.on('error', function (err) {
+  util.errorHandler(err)
+})
+
+ws.on('message', function incoming (data) {
+  proxyMessage.message = JSON.parse(data)
+})
+
+ws.on('close', function close () {
+  console.log('Disconnected')
+})
 
 /*
 * Get configs
@@ -23,7 +61,7 @@ function asyncScanTasks (next) {
   async.mapLimit(configs, 3, asyncScans, (err) => {
     if (err) throw err
 
-    log('Number of imports', readyConfigs.length)
+    log('Number of imports:', readyConfigs.length)
 
     next()
   })
@@ -62,59 +100,13 @@ async function postScan (config) {
 }
 
 /*
-* Set up new Observable with Proxy inside for
-* ability to subscribe when import is complete
-* */
-let completedImports = []
-
-const observable = new Observable(subscriber => {
-  completedImports = new Proxy(completedImports, {
-    set: function (target, key, value) {
-      if (key === 'length') {
-        subscriber.next(target)
-      }
-
-      target[key] = value
-
-      return true
-    }
-  })
-})
-
-let api = util.removeHttp(util.config.apiUrl)
-
-/*
-* Init client for WebSocket
-* */
-const ws = new WebSocket(`ws://${api}/gekko_event`, '', {})
-
-ws.on('error', function (err) {
-  util.errorHandler(err)
-})
-
-ws.on('message', function incoming (data) {
-  let responce = JSON.parse(data)
-
-  log(chalk.blue(`Import with ID ${responce.import_id} in progress`))
-
-  // Push completed ID of backtests into the Proxy's array
-  if (responce.updates && responce.updates.done === true) {
-    completedImports.push(responce.import_id)
-  }
-})
-
-ws.on('close', function close () {
-  console.log('Disconnected')
-})
-
-/*
 * Start imports with given concurrency
 * */
 function asyncImportTasks (next) {
   async.mapLimit(readyConfigs, util.config.parallelQueries, asyncImports, (err) => {
     if (err) throw err
 
-    log('All imports are complete')
+    log(chalk.green('All imports are complete'))
 
     ws.close(1000, 'All imports are done')
 
@@ -129,9 +121,7 @@ async function asyncImports (config) {
   try {
     let startedImportID = await postImport(config)
 
-    let completedImport = await listenImports(startedImportID)
-
-    log(chalk.green(`Import with ID ${completedImport.id} completed`))
+    let completedImport = await listenImports(startedImportID, config)
   } catch (err) {
     util.errorHandler(err)
   }
@@ -142,24 +132,56 @@ async function asyncImports (config) {
 * */
 async function postImport (config) {
   return axios.post(`${util.config.apiUrl}/api/import`, config).then((response) => {
-    log(chalk.cyanBright(`Import ${response.data.watch.exchange} ${response.data.watch.currency}/${response.data.watch.asset} started`))
-    log(chalk.cyanBright(`From ${response.data.from} to ${response.data.to} ID: ${response.data.id}`))
-
     return response.data
   })
 }
 
 /*
+* Count progress of current import
+* All parameters in UTC
+* */
+function countProgressOfImport (from, to, latest) {
+  let timespan = to.diff(from)
+  let fromEndMs = to.diff(latest)
+  let current = timespan - fromEndMs
+
+  return 100 * current / timespan
+}
+
+/*
 * Subscribe and listen if our started import finished
 * */
-async function listenImports (currentImport) {
+async function listenImports (currentImport, config) {
   return new Promise(
     (resolve, reject) => {
-      const subscription = observable.subscribe(comletedImports => {
-        if (_.includes(comletedImports, currentImport.id)) {
-          subscription.unsubscribe()
+      let from = moment.utc(config.importer.daterange.from)
+      let to = moment.utc(config.importer.daterange.to)
+      let bar = new ProgressBar({
+        schema: ':bar',
+        total: 100
+      })
+      let firstLaunch = true
 
-          resolve(currentImport)
+      const msgSubscription = importMessageObservable.subscribe((message) => {
+        if (message.import_id === currentImport.id) {
+          if (firstLaunch) {
+            log(chalk.cyanBright(`Import of ${_.capitalize(config.watch.exchange)} ${config.watch.currency}/${config.watch.asset} started`))
+            log(chalk.cyanBright(`From ${config.importer.daterange.from} to ${config.importer.daterange.to}`))
+            firstLaunch = false
+          }
+
+          let latest = moment.utc(message.updates.latest)
+
+          let progress = countProgressOfImport(from, to, latest)
+
+          if (message.updates.done === true) {
+            bar.update(1)
+
+            msgSubscription.unsubscribe()
+            resolve(currentImport)
+          } else {
+            bar.update(progress / 100)
+          }
         }
       })
     }
